e9ca50128a7cd06a76aee458c92e65ff28b080af
Hanly De Los Santos <hdelossantos@wisc.edu>
2011-02-25 21:42:06 +0000

    Made some changes to debug

--- src/main/java/loci/wiscscan/auth/KerberosAuth.java
+++ src/main/java/loci/wiscscan/auth/KerberosAuth.java
@@ -3,9 +3,9 @@
 //
 
 /*
-Java code for use with WiscScan.
+Utility code for use with WiscScan.
 
-Copyright (c) 2008, UW-Madison LOCI
+Copyright (c) 2011, UW-Madison LOCI
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -36,6 +36,7 @@ package loci.wiscscan.auth;
 
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
+import javax.swing.JOptionPane;
 
 /**
  * Code to manage kerberos user authentication in WiscScan.
@@ -56,34 +57,39 @@ public class KerberosAuth {
 	 */
 	public static boolean tryLogin(String username, String password) {
 		boolean returnValue = true;
+		
+		JOptionPane.showMessageDialog(null,
+				"realm = " + System.getProperty("java.security.krb5.realm") +
+				"\nkdc = " + System.getProperty("java.security.krb5.kdc") +
+				"\nconfig = " + System.getProperty("java.security.auth.login.config"));
 
 		// Create the credentials file
-		Credentials credentials = new Credentials();
-		credentials.setM_username(username);
-		credentials.setM_password(password);
-
-		LoginContext lc = null;
-		try {
-			lc = new LoginContext("WiscScanLogin", new AutoLoginHandler(credentials));
-		}
-		catch (LoginException le) {
-			System.err.println("Cannot create LoginContext. " + le.getMessage());
-			returnValue = false;
-		}
-		catch (SecurityException se) {
-			System.err.println("Cannot create LoginContext. " + se.getMessage());
-			returnValue = false;
-		}
-
-		try {
-			// attempt authentication
-			lc.login();
-		}
-		catch (LoginException le) {
-			System.err.println("Authentication failed:");
-			System.err.println("  " + le.getMessage());
-			returnValue = false;
-		}
+//		Credentials credentials = new Credentials();
+//		credentials.setM_username(username);
+//		credentials.setM_password(password);
+//
+//		LoginContext lc = null;
+//		try {
+//			lc = new LoginContext("WiscScanLogin", new AutoLoginHandler(credentials));
+//		}
+//		catch (LoginException le) {
+//			System.err.println("Cannot create LoginContext. " + le.getMessage());
+//			returnValue = false;
+//		}
+//		catch (SecurityException se) {
+//			System.err.println("Cannot create LoginContext. " + se.getMessage());
+//			returnValue = false;
+//		}
+//
+//		try {
+//			// attempt authentication
+//			lc.login();
+//		}
+//		catch (LoginException le) {
+//			System.err.println("Authentication failed:");
+//			System.err.println("  " + le.getMessage());
+//			returnValue = false;
+//		}
 
 		return returnValue;
 	}
